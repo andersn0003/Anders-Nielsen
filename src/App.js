a4b24e849e8ebd2e8f8b8e6948d402b3828e7e95
andersn0003 <andersn0003@gmail.com>
2024-10-15 06:41:47 +1000

    aupdate2

--- src/App.js
+++ src/App.js
@@ -16,9 +16,9 @@ function App() {
 
   return (
     <div className="flex flex-row w-screen h-screen justify-center px-[15%] max-2xl:px-[10%] max-xl:px-[5%] max-[970px]:px-0 max-[860px]:flex-col ">
-      <div className='w-[40%] h-full bg-gradient-to-br from-white via-gray-500 to-white px-[1px] shadow-[5px_0px_10px] z-30 max-[860px]:hidden'>
+      <div className='w-[30%] h-full bg-gray-300 px-[1px] z-30 max-[860px]:hidden'>
         <header className="w-full h-full  bg-[rgba(255_255_255_55)] py-16 px-8 flex flex-col items-center gap-5">
-          <img src='/img/avatar.png' alt="" className='rounded-full shadow-md' />
+          <img src='/img/avatar1.png' alt="" className='rounded-full shadow-md' />
           <div className='text-[36px] text-center hover:drop-shadow-[0px_0px_3px_gray]'>Anders Nielsen</div>
           <div className='text-center text-[20px] font-serif font-semibold'>Kyiv, Ukraine</div>
           <LinkBox />
@@ -26,10 +26,10 @@ function App() {
           <Interests />
         </header>
       </div>
-      <div className='hidden max-[860px]:flex flex-row items-center justify-between gap-[4%] sticky top-0 px-10 py-4 bg-gradient-to-bl from-slate-500 via-white to-slate-500 opacity-90 shadow-[0px_5px_10px] z-40'>
+      <div className='hidden max-[860px]:flex flex-row items-center justify-between gap-[4%] sticky top-0 px-10 py-4 bg-gray-300 z-40'>
         <div className='flex flex-col items-center'>
-          <img src='/img/avatar.png' alt="" className='rounded-full shadow-md w-40 h-40 max-[470px]:w-32 max-[470px]:h-32' />
-          <img src="https://readme-typing-svg.herokuapp.com?font=Serif&size=30&color=15F748FF&pause=3000&size=32&center=true&vCenter=true&width=300&height=50&lines=Anders+Nielson;Kyiv+Ukraine;Co-Founder;Program+Engineer" alt="Headline" />
+          <img src='/img/avatar1.png' alt="" className='rounded-full shadow-md w-40 h-40 max-[470px]:w-32 max-[470px]:h-32' />
+          <img src="https://readme-typing-svg.herokuapp.com?font=Serif&size=30&color=FFFFFFFF&pause=3000&size=32&center=true&vCenter=true&width=300&height=50&lines=Anders+Nielson;Kyiv+Ukraine;Co-Founder;Program+Engineer" alt="Headline" />
           <LinkBox />
         </div>
         <img src="https://readme-typing-svg.herokuapp.com?font=Fira+Code&size=14&pause=1000&color=15F748&center=false&vCenter=true&multiline=true&width=300&height=150&lines=Non-Fungible+Token;Trading;DeFi/DEX;Cross+Chain;Generative+AI" alt="Typing SVG" />
